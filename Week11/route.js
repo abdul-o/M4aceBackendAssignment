245561a6eda1ace1b492342c862ecbc9fa8cb643
Abdul-o <abdullateefdauda01@gmail.com>
2025-05-10 17:36:40 +0100

    week11

---
+++ Week11/route.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Welcome to My Server!');
+  } else if (req.url === '/about') {
+    res.end('This is the About page.');
+  } else if (req.url === '/contact') {
+    res.end('Contact us at: contact@example.com');
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('404 - Page Not Found');
+  }
+});
+
+const PORT = 3000;
+server.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
