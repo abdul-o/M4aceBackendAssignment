ac35bf9648ae102523b1a3df9df8678e7bcc2c97
Abdul-o <abdullateefdauda01@gmail.com>
2025-05-11 15:31:14 +0100

    Week 11 adjusted

--- Week11/route.js
+++ Week11/route.js
@@ -1,15 +1,17 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-
   if (req.url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Welcome to My Server!');
   } else if (req.url === '/about') {
-    res.end('This is the About page.');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Willing to know more about me. Stay tune.');
   } else if (req.url === '/contact') {
-    res.end('Contact us at: contact@example.com');
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('You can contact me at: abdullateefdauda01@gmail.com');
   } else {
+    // Make sure to write headers and end only once here
     res.writeHead(404, { 'Content-Type': 'text/plain' });
     res.end('404 - Page Not Found');
   }
